3448859f7d7a9f6a29de91e1de70f8d48885d8fa
Luis Orduz <lo@luord.com>
2017-06-25 12:25:40 -0500

    Added venv module, as it could be handy

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 apt update
-apt install -y --no-install-recommends python3-pip python3-setuptools python3-wheel
+apt install -y --no-install-recommends python3-pip python3-setuptools python3-wheel python3-venv
 
 apt clean
 apt autoclean
